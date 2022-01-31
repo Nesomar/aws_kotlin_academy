705e5cfb495de6dea60669443290780e42d38d1c
Nesomar Ramalho Freire <nfreire@latam.stefanini.com>
2022-01-31 20:26:09 -0300

    Estrutura inicial do projeto

---
+++ src/main/kotlin/br/com/academy/awskotlin/AwskotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.academy.awskotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwskotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwskotlinApplication>(*args)
+}
